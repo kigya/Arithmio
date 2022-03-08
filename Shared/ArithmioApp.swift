f8d0af96b5679f840f88d5768ac8eee839f4123a
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-03-08 22:28:00 +0300

    Initial commit

---
+++ Shared/ArithmioApp.swift
@@ -0,0 +1,18 @@
+//
+//  ArithmioApp.swift
+//  Shared
+//
+//  Created by Kirill Borichevskiy on 8.03.22.
+//
+//
+
+import SwiftUI
+
+@main
+struct ArithmioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
